9d3688f5dfd5f81b42bb212fcc338a32027bc1d8
Dave Hoffman <spaghettidave80@gmail.com>
2018-12-04 09:31:18 -0500

    SignIn page layout finished.

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -4,15 +4,19 @@ import './SignIn.css';
 
 class SignIn extends Component {
   state = {
-
+    email: '',
+    password: ''
   }
 
   handleChange = (e) => {
-    console.log(e);
+    this.setState({
+      [e.target.id]: e.target.value
+    });
   }
 
   handleSubmit = (e) => {
-    console.log(e);
+    e.preventDefault();
+    console.log(this.state);
   }
 
   render() {
